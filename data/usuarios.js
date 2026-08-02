1013b10963076f546720447586eb810d0ed87e58
thyagoosk8 <47331726+thyagoosk8@users.noreply.github.com>
2026-08-02 13:27:20 -0300

    Atualizar o usuarios.js

--- data/usuarios.js
+++ data/usuarios.js
@@ -1,1 +1,14 @@
-    
+    const usuarios = {
+  usuarioCadastro: {
+    nome: "João da Silva",
+    email: "joao@email.com",
+    senha: "Senha@123"
+  },
+
+  usuarioPadrao: {
+    email: "usuario.padrao@sistema.com",
+    senha: "SenhaSegura123!"
+  }
+};
+
+module.exports = usuarios;
