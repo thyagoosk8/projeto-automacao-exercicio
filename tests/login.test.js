3f3f2621c325cc6916dd6ca5159c8fc40062387c
thyagoosk8 <47331726+thyagoosk8@users.noreply.github.com>
2026-08-02 13:31:08 -0300

    Atualizar o login.test.js

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,1 +1,16 @@
-    
+const SistemaService = require("../services/SistemaService");
+const CadastroPage = require("../pages/CadastroPage");
+const usuarios = require("../data/usuarios");
+
+const service = new SistemaService();
+const cadastro = new CadastroPage(service);
+
+console.log("Teste: Cadastro com sucesso");
+
+const resposta = cadastro.cadastrar(usuarios.usuarioCadastro);
+
+if (resposta.status === 201) {
+  console.log("✅ Cadastro realizado com sucesso!");
+} else {
+  console.log("❌ Falha no cadastro.");
+}
