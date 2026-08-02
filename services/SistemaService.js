85d21b79fa9559ccb42917880c3bae0c3a4c8dbf
thyagoosk8 <47331726+thyagoosk8@users.noreply.github.com>
2026-08-02 13:28:02 -0300

    Atualizar o SistemaService.js

--- services/SistemaService.js
+++ services/SistemaService.js
@@ -1,1 +1,36 @@
-    
+    class SistemaService {
+
+  cadastrar(usuario) {
+    if (!usuario.nome || !usuario.email || !usuario.senha) {
+      return {
+        status: 400,
+        mensagem: "Todos os campos de cadastro são obrigatórios"
+      };
+    }
+
+    return {
+      status: 201,
+      id: 999
+    };
+  }
+
+  login(email, senha) {
+    if (
+      email === "usuario.padrao@sistema.com" &&
+      senha === "SenhaSegura123!"
+    ) {
+      return {
+        status: 200,
+        token: "token_seguro_123"
+      };
+    }
+
+    return {
+      status: 401,
+      erro: "Credenciais inválidas"
+    };
+  }
+
+}
+
+module.exports = SistemaService;
